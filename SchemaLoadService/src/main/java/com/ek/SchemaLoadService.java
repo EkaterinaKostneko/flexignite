50aecc366595ad59fe51ee27a6cf47af0256ccf3
kostenko <kostenko.ekaterina@gmail.com>
2024-09-25 23:05:13 +0900

    init commit

---
+++ SchemaLoadService/src/main/java/com/ek/SchemaLoadService.java
@@ -0,0 +1,11 @@
+package com.ek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaLoadService {
+    public static void main(String[] args) {
+        SpringApplication.run(SchemaLoadService.class, args);
+    }
+}

3d5acc7b7c10c06fd5a0b8af154c42cc63296354
Aleksandr Sitkov <a.sitkov@bftcom.com>
2024-10-30 09:59:55 +0300

    query saving

--- SchemaLoadService/src/main/java/com/ek/configuration/AppConfiguration.java
+++ SchemaLoadService/src/main/java/com/ek/configuration/AppConfiguration.java
@@ -3,42 +3,54 @@ package com.ek.configuration;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.QueryEntity;
+import org.apache.ignite.cache.QueryIndex;
+import org.apache.ignite.calcite.CalciteQueryEngineConfiguration;
 import org.apache.ignite.configuration.CacheConfiguration;
-import org.apache.ignite.configuration.ClientConfiguration;
+import org.apache.ignite.configuration.ClientConnectorConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
-//import org.apache.ignite.spi.tracing.opencensus.OpenCensusTracingSpi;
+import org.apache.ignite.configuration.SqlConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 @Configuration
 public class AppConfiguration {
-//    @Bean
-//    public IgniteCache<?,?> igniteCache () {
-//        // Starting Ignite
-//        IgniteConfiguration cfg = new IgniteConfiguration();
-//
-//        cfg.setClientMode(true);
-//        cfg.setPeerClassLoadingEnabled(true);
-//        cfg.setTracingSpi(new OpenCensusTracingSpi());
-//
-//        // Setting up an IP Finder to ensure the client can locate the servers.
-//        TcpDiscoveryMulticastIpFinder ipFinder = new TcpDiscoveryMulticastIpFinder();
-//        ipFinder.setAddresses(Collections.singletonList("109.172.89.16:47500"));
-//        cfg.setDiscoverySpi(new TcpDiscoverySpi().setIpFinder(ipFinder));
-//
-//        Ignite ignite = Ignition.start(cfg);
-//
-//        return ignite.getOrCreateCache(new CacheConfiguration("dynamicCache"));
-//    }
 
     @Bean
-    public ClientConfiguration clientConfiguration() {
+    public Ignite ignite() {
 
-        return new ClientConfiguration()
-            .setAddresses("109.172.89.16:10800");
+        IgniteConfiguration igniteCfg = new IgniteConfiguration();
+
+        ClientConnectorConfiguration ccfg = new ClientConnectorConfiguration();
+        ccfg.setHost("127.0.0.1");
+        ccfg.setPort(10800);
+        ccfg.setPortRange(0);
+
+// Set client connection configuration in IgniteConfiguration
+        igniteCfg.setClientConnectorConfiguration(ccfg);
+        igniteCfg.setSqlConfiguration(
+            new SqlConfiguration().setQueryEnginesConfiguration(
+                new CalciteQueryEngineConfiguration().setDefault(true)
+            )
+        );
+
+// Start Ignite node
+        return Ignition.start(igniteCfg);
+    }
+
+    @Bean
+    public IgniteCache<Long, Object> igniteCache(Ignite ignite) {
+
+        CacheConfiguration<Long, Object> personCacheCfg = new CacheConfiguration<Long, Object>();
+        personCacheCfg.setName("dynamicTable");
+
+        QueryEntity queryEntity = new QueryEntity(Long.class.getName(), "dynamicCache")
+            .addQueryField("id", Long.class.getName(), null)
+            .addQueryField("field", String.class.getName(), null);
+        queryEntity.setIndexes(Arrays.asList(new QueryIndex("id")));
+
+        return ignite.getOrCreateCache(personCacheCfg);
     }
 }
--- SchemaLoadService/src/main/java/com/ek/service/SchemaLoadService.java
+++ SchemaLoadService/src/main/java/com/ek/service/SchemaLoadService.java
@@ -2,66 +2,39 @@ package com.ek.service;
 
 
 import com.ek.SqlFieldTypes;
-import com.ek.dto.Field;
 import com.ek.dto.TableFields;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
-import org.apache.ignite.client.ClientCache;
-import org.apache.ignite.client.IgniteClient;
-import org.apache.ignite.configuration.ClientConfiguration;
-import org.apache.ignite.configuration.ClientConnectorConfiguration;
-import org.apache.ignite.configuration.IgniteConfiguration;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SchemaLoadService {
 
-    private final ClientConfiguration clientConfiguration;
+    private final IgniteCache<Long, Object> igniteCache;
 
     public void createSchema(Map<String, Map<String, TableFields>> models) {
 
-        IgniteConfiguration igniteCfg = new IgniteConfiguration();
+        Map<String, TableFields> schemas = models.get("schemas");
 
-        ClientConnectorConfiguration ccfg = new ClientConnectorConfiguration();
-        ccfg.setHost("127.0.0.1");
-        ccfg.setPort(10800);
-        ccfg.setPortRange(0);
+        for (Map.Entry<String, TableFields> stringTableFieldsEntry : schemas.entrySet()) {
 
-// Set client connection configuration in IgniteConfiguration
-        igniteCfg.setClientConnectorConfiguration(ccfg);
+            String tableName = stringTableFieldsEntry.getKey();
+            TableFields fields = stringTableFieldsEntry.getValue();
 
-// Start Ignite node
-        Ignition.start(igniteCfg);
+            String query = String.format("CREATE TABLE IF NOT EXISTS %s (%s)", tableName, createFieldsQuery(fields));
 
+            log.warn("QUERY to SAVE - {}", query);
 
-        try (IgniteClient client = Ignition.startClient(clientConfiguration)) {
-            ClientCache<Integer, String> igniteCache = client.getOrCreateCache("myCache");
- //      ClientCache<Integer, String> igniteCache = client.cache("dynamicCache");
-
-            Map<String, TableFields> schemas = models.get("schemas");
-            for (Map.Entry<String, TableFields> stringTableFieldsEntry : schemas.entrySet()) {
-                String tableName = stringTableFieldsEntry.getKey();
-                TableFields fields = stringTableFieldsEntry.getValue();
-
-         //       igniteCache.query(new SqlFieldsQuery("CREATE TABLE ? (?)").setArgs(tableName, createFieldsQuery(fields))).getAll();
-                  igniteCache.query(new SqlFieldsQuery(
-                          "CREATE TABLE IF NOT EXISTS dynamicTable (id LONG PRIMARY KEY, field varchar")).getAll();
-
-
-            }
-               System.out.println(igniteCache.getName());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            igniteCache.query(new SqlFieldsQuery(query)).getAll();
         }
     }
-
     private String createFieldsQuery(TableFields tableFields) {
         return tableFields
             .getProperties()
